46ee16730a8a808fca51bf42d002a6969ccbde64
Brian Kemper <bkemper@gmail.com>
2015-05-13 16:05:35 -0600

    pages

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+    $("a[href=#menuExpand]").click(function(e) {
+        $(".outer").toggleClass("grow");
+        e.preventDefault();
+    });
+});
+
+$(function() {
+    $("a[href=#menuExpand]").click(function(e) {
+        $(".menu").toggleClass("menuOpen");
+        e.preventDefault();
+    });
+});
+
+$(".rotate").click(function(){
+ $(this).toggleClass("down")  ; 
+})
