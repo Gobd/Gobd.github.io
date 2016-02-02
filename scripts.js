55e1f54e6d9a9b5d59a47f701ab108a7e64d6c47
Brian Kemper <bkemper@gmail.com>
2016-02-02 16:14:20 -0700

    rewrote website, same thing just cleaned up

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 $(function() {
-    $("a[href=#menuExpand]").click(function(e) {
-        $(".outer").toggleClass("grow");
-        e.preventDefault();
-    });
+  $("a[href=#menuExpand]").click(function(e) {
+    $(".outer").toggleClass("grow");
+    e.preventDefault();
+  });
 });
 
 $(function() {
-    $("a[href=#menuExpand]").click(function(e) {
-        $(".menu").toggleClass("menuOpen");
-        e.preventDefault();
-    });
+  $("a[href=#menuExpand]").click(function(e) {
+    $(".menu").toggleClass("menuOpen");
+    e.preventDefault();
+  });
 });
 
-$(".rotate").click(function(){
- $(this).toggleClass("down")  ; 
-})
+$(".rotate").click(function() {
+  $(this).toggleClass("down");
+});
